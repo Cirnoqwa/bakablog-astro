8b4ba91f390aec97ce7f7b842cef797ccbfb6eff
Cirnoqwa <3059630032@qq.com>
2026-08-11 11:26:45 +0800

    giscus 组件更新

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,9 @@ const postsCollection = defineCollection({
 		category: z.string().optional().nullable().default(""),
 		lang: z.string().optional().default(""),
 
+		/* 评论 */
+		comment: z.boolean().optional().default(true),
+
 		/* For internal use */
 		prevTitle: z.string().default(""),
 		prevSlug: z.string().default(""),
